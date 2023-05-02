64d556e9c460f836ccbc2f972e78990d7748e591
HobGoblin94 <amin.al.adeeb@gmail.com>
2023-05-02 19:13:20 +0800

    done

---
+++ app.py
@@ -0,0 +1,75 @@
+from urllib import response
+from flask import Flask, jsonify, render_template, request
+from services.ssl_checker import get_expiry_dates
+import re
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template(
+            "index.html"
+        )
+    elif request.method == "POST":
+        # Get Form Data
+        hostname = request.form['hostname']
+        # strip fqdn trimming
+        url = re.compile(r"https?://(www\.)?")
+        hostname = url.sub('', hostname).strip().strip('/')
+        # get expiry data
+        expiry_payload_data = get_expiry_dates(hostname)
+        if expiry_payload_data["message"] == True:
+            return render_template(
+                "index_results.html",
+                cert_hostname = hostname,
+                cert_issuer = expiry_payload_data["issuer"],
+                cert_start_date = expiry_payload_data["start_date"],
+                cert_end_date = expiry_payload_data["end_date"],
+                cert_expiry_days = expiry_payload_data["days_diff"],
+                cert_status = expiry_payload_data["expiry_status"]
+            )
+
+        else:
+            return render_template(
+                "index_results_error.html",
+                error_message = expiry_payload_data["message"]
+            )
+    else:
+        return jsonify(
+            message="No valid method has been sent"
+        )
+
+@app.route('/api/v1/cert_check/check', methods=["GET", "POST"])
+def cert_check():
+    if request.method == "GET":
+        return jsonify(
+            # Implement health checker
+            health_check="UP"
+        )
+    elif request.method == "POST":
+
+        hostname = request.json['hostname']
+        # strip fqdn trimming
+        url = re.compile(r"https?://(www\.)?")
+        hostname = url.sub('', hostname).strip().strip('/')
+        # get expiry data
+        expiry_payload_data = get_expiry_dates(hostname)
+
+        return jsonify(
+            cert_hostname = hostname,
+            cert_issuer = expiry_payload_data["issuer"],
+            cert_start_date = expiry_payload_data["start_date"],
+            cert_end_date = expiry_payload_data["end_date"],
+            cert_expiry_days = expiry_payload_data["days_diff"],
+            cert_status = expiry_payload_data["expiry_status"]
+            
+        )
+    else:
+        return jsonify(
+            message="No valid method has been sent"
+        )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
---
+++ services/ssl_checker.py
@@ -0,0 +1,41 @@
+from urllib.request import ssl, socket
+import validators
+from datetime import date, datetime
+
+
+def get_expiry_dates(hostname):
+    expiry_payload_data = {
+        "start_date":None,
+        "end_date":None,
+        "message":None,
+        "days_diff":None,
+        "expiry_status":None,
+  "issuer": None
+    }
+    port = '443'
+    context = ssl.create_default_context()
+    if validators.domain(hostname):
+        try:
+            with socket.create_connection((hostname, port)) as sock:
+                with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+                    dict_data = ssock.getpeercert()
+                    # available keys: ['subject', 'issuer', 'version', 'serialNumber', 'notBefore', 'notAfter', 'subjectAltName', 'OCSP', 'caIssuers', 'crlDistributionPoints']
+                    expiry_payload_data["start_date"] = dict_data['notBefore']
+                    expiry_payload_data["end_date"] = dict_data['notAfter']
+                    expiry_payload_data["issuer"] = dict_data['issuer']
+                    expiry_payload_data["message"] = True
+                    date_today = datetime.today()
+                    end_date_to_object = datetime.strptime(expiry_payload_data["end_date"], '%b %d %H:%M:%S %Y %Z')
+                    if end_date_to_object >= date_today:
+                        days_diff = end_date_to_object - date_today
+                        expiry_payload_data["days_diff"] =  days_diff.days
+                        expiry_payload_data["expiry_status"] = "not expired"
+                    else:
+                        days_diff = end_date_to_object - date_today
+                        expiry_payload_data["days_diff"] =  days_diff.days
+                        expiry_payload_data["expiry_status"] = "expired"
+        except Exception as error:
+            expiry_payload_data["message"] = error
+    else:
+        expiry_payload_data["message"] = "Not valid domain"
+    return expiry_payload_data
